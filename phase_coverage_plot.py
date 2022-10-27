40706ec30d364d67364b82995f6a6e0cdabe5379
ajm855 <ajm855@mail.usask.ca>
2022-10-26 21:08:43 -0600

    Improved readability

--- phase_coverage_plot.py
+++ phase_coverage_plot.py
@@ -20,6 +20,13 @@ def phase(jd, p):
     """
     return (jd % p) / p
 
+def utc_to_jd(utc_date):
+    """
+    :param utc_date: Julian date to be converted.
+    :return: Float. Converted Julian date.
+    """
+    t = Time(utc_date, scale='utc')
+    return t.jd
 
 def add_prediction(plt_ax, p, jd_start, jd_end, colour='r'):
     """
@@ -65,11 +72,7 @@ def graph_phase_coverage(plt_ax, p, infolder):
 
     plt_ax.scatter(phase_list, date_list, s=1, zorder=4)
 
-def utc_to_jd(utc_date):
-    t = Time(utc_date, scale='utc')
-    return t.jd
-
-def find_phase_at_sunset(target_phase_at_sunset, p, sky_obj, threshold=0.01):
+def find_phase_at_sunset(target_phase_at_sunset, p, sky_obj, threshold=0.01, verbose=True):
     """
     :param target_phase_at_sunset: Float. Desired phase at sunset for given object with period p.
     :param p: Float. Period to phase by.
@@ -77,57 +80,59 @@ def find_phase_at_sunset(target_phase_at_sunset, p, sky_obj, threshold=0.01):
     :return: datetime date. First date found at which the day's phase at sunset is within threshold of target_phase_at_sunset. 
     Returns -1 if nothing is found within one year.
     """
-    # Setup Saskatoon sun
+    # Set up Saskatoon sun for sunset time calculation.
     latitude = 52.146973
     longitude = -106.647034
     altitude = 482 # meters
     sun = SunTimes(latitude=latitude, longitude=longitude, altitude=altitude)
+
+    # Set up observatory location and object information for alitude/elevation calculation.
+    obj = sky_obj
+    saskatoon = EarthLocation(lat= latitude*u.deg, lon=longitude*u.deg, height=altitude*u.m )
 
     # Start iterating from today, end on the same date next year.
     date_iter = datetime.utcnow().date()
     next_year = date(date_iter.year+1, date_iter.month, date_iter.day)
     
     while date_iter < next_year :
-        # Calculate time of sunset in UTC at Saskatoon
+        # Calculate time of sunset in UTC at Saskatoon.
         sunset = sun.setutc(date_iter)
-        #Calculate phase of object at sunset
+
+        #Calculate phase of object at sunset.
         iter_phase_at_sunset = phase(utc_to_jd(sunset), p)
 
-        
-
-        # If phase is within threshold of desired phase, return date. 
-        # Handling for cases near 0 and 1
+        # If phase is within threshold of desired phase, continue. 
+        # Handling for cases where phase is near 0 and 1, threshold needs to "wrap" over.
         wrap_up = iter_phase_at_sunset + threshold - 1
         wrap_down = iter_phase_at_sunset - threshold + 1
-
         if (
             (abs(iter_phase_at_sunset - target_phase_at_sunset) < threshold) or 
             (wrap_up > 0 and wrap_up > target_phase_at_sunset) or 
             (wrap_down < 1 and wrap_down < target_phase_at_sunset)
         ):
 
-            # Calculate altitude of given object at sunset
-            obj = sky_obj
-            saskatoon = EarthLocation(lat= latitude*u.deg, lon=longitude*u.deg, height=altitude*u.m )
+            # Calculate altitude of given object at sunset at observatory location.
             time = sunset
             obj_altaz = obj.transform_to(AltAz(obstime=time, location=saskatoon))
             obj_alt = float(f"{obj_altaz.alt}"[:-4])
-        
+            
+            # If altitude is greater than 20.0 degrees, return JD of sunrise and sunset.
             if obj_alt > 20.0:
                 tomorrow = date_iter + timedelta(days=1)
                 sunrise = sun.riseutc(tomorrow)
                 iter_phase_at_sunrise = phase(utc_to_jd(sunrise), p)
-            
-                print(date_iter, tomorrow)
-                print("The object's phase will be", iter_phase_at_sunset, "on",sunset,"UTC (which is sunset)."  )
-                print("At sunrise on", sunrise, "(the following day) the phase will be", iter_phase_at_sunrise,".")
-                print("At sunset, the object will be at an altitude of", obj_alt)
 
+                if verbose:
+                    print(date_iter, tomorrow)
+                    print("At sunset, the object's phase will be", iter_phase_at_sunset, "occuring on", sunset,"UTC. It's altitude at this time is", obj_alt, "deg."  )
+                    print("At sunrise the following morning, the object's phase will be", iter_phase_at_sunrise, "occuring on", sunrise, "UTC.")
                 return [utc_to_jd(sunset), utc_to_jd(sunrise)]
-
+        # Iterate date.
         date_iter += timedelta(days=1)
-    print("Couldn't find required phase within a year.")
-    return -1
+    
+    # No sunset within phase threshold and elevation constraints for this observatory over the next year. 
+    print("Couldn't find required phase within threshold and elevation constraints within a year.")
+    return [-1,-1]
 
 
 # Initialize plot.
@@ -142,8 +147,8 @@ plt.ylabel("Date, MMDD (UTC)")
 plt.grid(visible=True)
 
 # Build coverage plot from observation files.
-period = 3.96004
-obs_folder = 'BetaAurSp'
+#period = 3.96004
+#obs_folder = 'BetaAurSp'
 #graph_phase_coverage(ax1, period, obs_folder)
 
 # Add predictions to coverage plot.
@@ -152,10 +157,12 @@ obs_folder = 'BetaAurSp'
 #add_prediction(ax1, period, 2459881.58333, 2459882.02083, 'g')
 #add_prediction(ax1, period, 2459882.58333, 2459883.02083, 'y')
 
-results = find_phase_at_sunset(0.1, 3.96004, SkyCoord.from_name("beta aurigae"))
-add_prediction(ax1, period, results[0], results[1])
-plt.show()
+# Search for desired phase at sunset, add predictions to plot.
+#results = find_phase_at_sunset(0.1, 3.96004, SkyCoord.from_name("beta aurigae"))
+#add_prediction(ax1, period, results[0], results[1])
+#plt.show()
 
+# Testing for altitude calculation.
 """
 ba = SkyCoord.from_name('beta aurigae')
 saskatoon = EarthLocation(lat= 52.146973*u.deg, lon=-106.647034*u.deg, height=482*u.m )
@@ -163,5 +170,6 @@ time = Time.now()
 
 ba_altaz = ba.transform_to(AltAz(obstime=time, location=saskatoon))
 print(f"BetaAur's Altitude = {ba_altaz.alt:.4}")
-print(float(f"{ba_altaz.alt}"[:-4])+1)
+print(ba_altaz.alt)
+print(float(f"{ba_altaz.alt}"[:-4]))
 """
